490e9359d71643f6ceb913bd061a9c99f10b54c3
inmortalkaktus <alejandroolivaresmompo@hotmail.com>
2022-04-19 16:37:49 +0200

    Create paginated pages

--- src/elements/PaginationElements.js
+++ src/elements/PaginationElements.js
@@ -11,17 +11,17 @@ export const PaginationWrapper = styled.div`
 
     a:nth-child(1) {
         color: ${props => 
-            props.isFirst ? props.theme.colors.dark3 : props.theme.colors.dark1}
-        pointer-events: ${props => (props.isFirst ? "none" : "auto")}
-        cursor: ${props => (props.isFirst ? "default" : "pointer")}
+            props.isFirst ? props.theme.colors.dark3 : props.theme.colors.dark1};
+        pointer-events: ${props => (props.isFirst ? "none" : "auto")};
+        cursor: ${props => (props.isFirst ? "default" : "pointer")};
     
     }
 
     a:nth-child(2) {
         color: ${props => 
-            props.isLast ? props.theme.colors.dark3 : props.theme.colors.dark1}
-        pointer-events: ${props => (props.isLast ? "none" : "auto")}
-        cursor: ${props => (props.isLast ? "default" : "pointer")}
+            props.isLast ? props.theme.colors.dark3 : props.theme.colors.dark1};
+        pointer-events: ${props => (props.isLast ? "none" : "auto")};
+        cursor: ${props => (props.isLast ? "default" : "pointer")};
     
     }
 
---
+++ src/templates/allPosts.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Container, Content, ContentCard, FeatureImage, Pagination } from "../components"
+import { H1, P } from "../elements"
+
+const allPosts = ({pageContext, data}) => {
+    const { currentPage, numPages } = pageContext
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`
+    const nextPage = `/${currentPage + 1}`
+
+    const posts = data.allMdx.edges
+
+    return (
+        <Container>
+            <FeatureImage />
+            <Content>
+                <H1 textAlign="center" margin="0 0 1rem 0">
+                    Some dummy text
+                </H1>
+                <P color="dark2" textAlign="center">
+                    Blandit eos eu lorem ut aliquam sit. Accusam ipsum et voluptua et consetetur duo. Dolor ipsum at no duo ex volutpat ipsum. Sea ipsum magna dolor nibh nulla et diam sanctus dolore exerci at magna gubergren kasd feugait et. Et dolor gubergren possim ut sadipscing adipiscing. Lorem feugiat stet consequat invidunt eirmod elitr in. Et rebum et sea sed lorem tation et consectetuer sed invidunt dolor vero sed justo dolor.
+                </P>
+                {posts.map(post => (
+                    <ContentCard 
+                        key={post.node.frontmatter.slug}
+                        date={post.node.frontmatter.date}
+                        title={post.node.frontmatter.title}
+                        excerpt={post.node.frontmatter.excerpt}
+                        slug={post.node.frontmatter.slug}
+                    />
+                ))}
+            </Content>
+            <Pagination 
+                isFirst={isFirst}
+                isLast={isLast}
+                prevPage={prevPage}
+                nextPage={nextPage}
+            />
+        </Container>
+    )
+
+
+}
+
+export default allPosts
+
+export const pageQuery = graphql`
+    query AllPostsQuery($skip: Int!, $limit: Int!) {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}, skip: $skip, limit: $limit) {
+            edges {
+                node {
+                    frontmatter {
+                        slug
+                        title
+                        date(formatString: "DD/MM/YYYY")
+                        excerpt
+                    }
+                }
+            }
+        }
+    }
+`
