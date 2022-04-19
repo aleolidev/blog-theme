c41cf1878894483f37cec40a7a8c99724a9b9e31
inmortalkaktus <alejandroolivaresmompo@hotmail.com>
2022-04-19 16:48:43 +0200

    Create single pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,14 +34,14 @@ exports.createPages = async ({ actions, graphql }) => {
     })
 
     // Create single blog posts
-    // data.allMdx.edges.forEach(edge => {
-    //     const slug = edge.node.frontmatter.slug
-    //     const id = edge.node.id
+    data.allMdx.edges.forEach(edge => {
+        const slug = edge.node.frontmatter.slug
+        const id = edge.node.id
 
-    //     actions.createPages({
-    //         path: slug,
-    //         component: require.resolve(`./src/templates/singlePost.js`),
-    //         context: { id },
-    //     })
-    // })
+        actions.createPage({
+            path: slug,
+            component: require.resolve(`./src/templates/singlePost.js`),
+            context: { id },
+        })
+    })
 }
